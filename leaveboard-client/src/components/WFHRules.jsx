8bfea8484e397d2be77180ca6a60cc8e006e9264
Aung <aung@digithaigroup.com>
2026-08-11 13:36:08 +0700

    Update today indicator and Fix double indent in WFH rules

--- leaveboard-client/src/components/WFHRules.jsx
+++ leaveboard-client/src/components/WFHRules.jsx
@@ -126,17 +126,11 @@ const WFHRules = () => {
           WFH requests for that week.
         </li>
         <li>{renderConcurrencyRule()}</li>
-        {concurrencyEntries.length > 0 && (
-          <li>
-            <ul>
-              {concurrencyEntries.map(([position, value]) => (
-                <li key={position}>
-                  {position}: up to {value} user(s) WFH on the same day
-                </li>
-              ))}
-            </ul>
+        {concurrencyEntries.map(([position, value]) => (
+          <li key={position}>
+            {position}: up to {value} user(s) WFH on the same day
           </li>
-        )}
+        ))}
       </ul>
     </section>
   );
